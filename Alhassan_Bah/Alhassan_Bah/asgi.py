07ecb8b1676b245cc64a47ae037d828519fda4ec
Alhassan-Zaria <93844602+Alhassan-Zaria@users.noreply.github.com>
2022-06-12 04:28:18 -0700

    Add files via upload

---
+++ Alhassan_Bah/Alhassan_Bah/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Alhassan_Bah project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Alhassan_Bah.settings')
+
+application = get_asgi_application()
